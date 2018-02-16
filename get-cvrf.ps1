97b405ceb4c809d3a6048ae1ac08209b2f83f215
VGG <vggchrome@gmail.com>
2018-02-16 16:03:16 -0500

    update #4

---
+++ get-cvrf.ps1
@@ -0,0 +1,13 @@
+﻿$api_key = "XXXX"
+Set-MSRCApiKey -ApiKey $api_key
+$months=Get-MsrcSecurityUpdate -After (Get-Date).AddDays(-60) -Before (Get-Date)
+foreach ($month in $months) 
+{
+    $month.ID
+    $cvrfDoc = Get-MsrcCvrfDocument -ID $month.ID | Where-Object -FilterScript { ($_.ProductTree.FullProductName.Value) -like "*server 2012*" }
+    $a=$cvrfDoc.Vulnerability.Remediations | where Type -eq 2
+    $a.Description.Value | Where-Object -filterscript { $_ -match '^\d' } | Get-Unique
+    
+}
+
+
